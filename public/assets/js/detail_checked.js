296597a89b68c04f4b51bf8d0d60d8fea6384682
nexchal <joamoon7@naver.com>
2019-10-30 14:56:27 +0900

    고장유형목록
    체크박스 생성하는 거 js파일에서 끌어다오기

--- public/assets/js/detail_checked.js
+++ public/assets/js/detail_checked.js
@@ -1,8 +1,8 @@
 function DetailChecked()
   {
-    var checkbox = document.getElementsByName("check");
-    var checkboxleng = checkbox.length;
-    var checked = 0;
+    var checkbox = document.getElementsByName("fault"); // 선택된 고장목록 체크박스 값
+    var checkboxleng = checkbox.length; // 선택된 체크박스 길이
+    var checked = 0; // 선택된 체크박스 개수
     var i = 0;
     for (i=0; i<checkboxleng; i++)
     {
@@ -12,7 +12,7 @@ function DetailChecked()
       }
     }
     document.frm.checked.value=checked;
-    if(checked == 20000)
+    if(checked == 20000) // 예외처리
     {
       alert("err");
     }
@@ -32,7 +32,7 @@ function DetailChecked()
     }
   }
 
-  function AddType()
+  function AddType() // 추가버튼 클릭 시 fault유형 목록들을 보여줌
    {
      var x = document.getElementById("myDIV");
      if (x.style.display === "none")
@@ -44,10 +44,10 @@ function DetailChecked()
        x.style.display = "none";
      }
 
-     show_box = document.getElementsByName("check"); //상세보기 페이지 내에 체크박스 이름은 check 임
-     val = document.getElementsByName("user_selected_value");
+     show_box = document.getElementsByName("fault"); // fault 유형 값을 가지고 있는 체크박스 name
+     val = document.getElementsByName("hidden_check"); // 현재 보고있는 사용자가 가지고있는 fault 유형 값
 
-     for(var i = 0; i < val.length; i++) // 사용자가 이전에 선택한 고장 목록은 체크되게함
+     for(var i = 0; i < val.length; i++) // 상세보기 페이지에서 사용자가 이전에 선택한 고장 목록은 체크되게함
      {
        nick = val[i].value-1
       show_box[nick].checked=true;
